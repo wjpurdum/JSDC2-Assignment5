81a2327ac28908b43e63fd508e8b61b3a6257273
wjpurdum <whitney.purdum@gmail.com>
2016-07-18 18:32:50 -0400

    corrections

--- js/index.js
+++ js/index.js
@@ -1,70 +1,65 @@
 //Model
 
-var input = [
-	
-	{
-		value: 0,
-		'above one-hundred': false,
-		'below-zero': false
-	},
+var input =
+// changed value to be a property
+    {
+        'value': 0,
+        'aboveOneHundred': false,
+        'belowZero': false
+    }
+  //removed comma
 
 
 //View - needs to reference template and compile - takes list up top and turns it into html
 function renderNumber() {
-$('#Number').html(input);
+$('#Number').html(input.value);
 var source = $('#countly-template').html();
+//removed input on click will render each individually
 var template = Handlebars.compile(source);
-
 }
 
-//Controller - listens for events and lists functions that need to happen 
+
+//Controller - listens for events and lists functions that need to happen
 
 
 function setup() {
-	
-	renderNumber();
 
-	$('#countUp').on('click', addOne);
-	$('#countDown').on('click', subtractOne);
-	$('#Reset').on('click', setToZero);
+    renderNumber();
+
+    $('#countUp').on('click', addOne);
+    $('#countDown').on('click', subtractOne);
+    $('#Reset').on('click', setToZero);
 }
 
 function addOne() {
-	var input = input += 1;
-	if input > 100 {
-		input.above one-hundred = true;
-	}
-	else if input <0 {
-		input.below-zero = true;
-	}
-
-}
+     input.value = input.value + 1 ;
+    if (input > 100) {
+  // put conditions in parenthesis
+        input.aboveOneHundred = true;
+    }
+    else if (input < 0) {
+        input.belowZero = true;
+    }
+  renderNumber();
+};
 
 function subtractOne() {
-	var input = input -= 1;
-	
-	if input > 100 {
-		input.above one-hundred = true;
-	}
-	else if input < 0 {
-		input.below-zero = true;
-	}
+    input.value = input.value - 1;
+    if (input.value > 100) {
+  //put conditions in parenthesis
+        input.aboveOneHundred = true;
+    }
+    else if (input < 0) {
+        input.belowZero = true;
+    }
 
-	renderNumber();
+    renderNumber();
 }
 
 function setToZero() {
-	var input = 0;
-	renderNumber();
-
-}
-
+    input.value = 0;
+    renderNumber();
+//removed additional parenthesis
 };
 
 $(document).ready(setup);
-
-
-
-
-
-
